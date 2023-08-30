6a4b859832251518dbb6efbefe15f05d5c7a8b58
monaeman <mona.eman2018@gmail.com>
2023-08-30 16:15:47 -0400

    mern app

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT|| 5000
+const {errorHandler} =require('./middleware/errorMiddleware')
+const colors = require('colors') //not required
+const app = express()
+const connectDB = require('./config/db')
+
+connectDB();
+//Middleware
+//this middleware used to get the body data
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+//use g
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+//user resource
+//comments resources
+//....
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
